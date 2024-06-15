987e70012e0d0b2fce16758d3d3629c35e675dc2
NGAN-NIGHT <pro193339@gmail.com>
2024-06-15 13:36:16 +0700

    Update Night.Hub.lua

--- Night.Hub.lua
+++ Night.Hub.lua
@@ -1,5 +1,5 @@
 ----------------------------------------------------------------------------------------------------------------------------------------------
-local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+local Fluent = loadstring(game:HttpGet("https://raw.githubusercontent.com/NGAN-NIGHT/Night-Hub-/main/Night.Hub.lua"))()
 local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
 ----------------------------------------------------------------------------------------------------------------------------------------------
